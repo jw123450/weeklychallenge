581dce1f1dc2d2b6343b66e826230843299bb403
jw123450 <156040007+jw123450@users.noreply.github.com>
2025-03-23 21:31:11 -1000

    sussy

--- hourspassed.py
+++ hourspassed.py
@@ -10,18 +10,18 @@ def hourspassed(hour1, hour2):
     else:
         rtime2 = int(time2[2]) + int(time2[4])/6 + int(time2[5])/60
     if 'PM' in hour1:
-	rtime1 += 12
+		rtime1 += 12
     if 'PM' in hour2:
-	rtime2 += 12
+		rtime2 += 12
     if rtime2 == rtime1:
-	print('No time has passed')
+		print('No time has passed')
     else:
-	ftime = rtime2 - rtime1
-	if ftime < 1:
-		print(round((ftime * 60), 1), 'Minutes passed')
-	else:
-		h, m = divmod(ftime, 1)
-		print(int(h), 'Hours', round((m * 60), 1), 'Minutes passed')
+		ftime = rtime2 - rtime1
+		if ftime < 1:
+			print(round((ftime * 60), 1), 'Minutes passed')
+		else:
+			h, m = divmod(ftime, 1)
+			print(int(h), 'Hours', round((m * 60), 1), 'Minutes passed')
 
 hourspassed("3:00 AM", "9:00 AM")
 hourspassed("2:00 PM", "4:00 PM")
