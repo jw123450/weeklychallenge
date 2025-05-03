ad8756584595fe57ef025a98afca4927c9916884
jw123450 <156040007+jw123450@users.noreply.github.com>
2025-05-03 09:53:54 -1000

    Create bucket.py

---
+++ bucket.py
@@ -0,0 +1,22 @@
+def buckets(orig, length):
+	l = orig.split(' ')
+	fl = []; c = -1; f = []
+	for n in l:
+		c += 1
+		if (length - c) <= len(n):
+			fs = (' ').join(f)
+			fl.append(fs)
+			f = []; c = -1;
+		if (length - c) > len(n):
+			f.append(n)
+			c += len(n)
+		else:
+			print('Not possible to bucket')
+	fs = (' ').join(f)
+	fl.append(fs)
+	print(fl)
+
+buckets("she sells sea shells by the sea", 10)
+buckets("the mouse jumped over the cheese", 7)
+buckets("fairy dust coated the air", 20)
+buckets("a b c d e", 2)
