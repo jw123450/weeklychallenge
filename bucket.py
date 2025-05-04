1e05747a4a1aeaf9a8cdc8784f61cdaab0db864c
jw123450 <156040007+jw123450@users.noreply.github.com>
2025-05-03 22:34:09 -1000

    Update bucket.py

--- bucket.py
+++ bucket.py
@@ -7,7 +7,7 @@ def buckets(orig, length):
 			fs = (' ').join(f)
 			fl.append(fs)
 			f = []; c = -1;
-		elif (length - c) > len(n):
+		if (length - c) > len(n):
 			f.append(n)
 			c += len(n)
 		else:
