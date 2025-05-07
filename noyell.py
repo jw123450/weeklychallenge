edb47cb09a689a4a21e2f8d0342bf46a042512cd
jw123450 <156040007+jw123450@users.noreply.github.com>
2025-05-06 14:03:21 -1000

    Create noyell.py

---
+++ noyell.py
@@ -0,0 +1,11 @@
+def noYelling(s):
+	n = s[-1]
+	while (s[-1] == '!') or (s[-1] == '?'):
+		s = s[:-1]
+	s = s+n
+	print(s)
+
+noYelling("What went wrong?????????")
+noYelling("Oh my goodness!!!")
+noYelling("I just!!! can!!! not!!! believe!!! it!!!")
+noYelling("Oh my goodness!")
